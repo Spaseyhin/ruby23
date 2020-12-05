db560456a0378f690b999bc21e52b7f94f2edb9c
Spasye <evgen.kalin99@gmail.com>
2020-12-05 21:12:10 +0300

    adding error notification

--- app.rb
+++ app.rb
@@ -23,17 +23,26 @@ post '/contacts' do
 	erb :cont
 end 
 get '/visit' do
+
 	erb :visit
 end
-get '/eee' do
-	erb :eee
-end
+
 post '/visit' do
+	
 	@username = params[:username]
 	@date = params[:date]
 	@phone = params[:phone]
 	@pric = params[:pric]
-
+	hh = {:username => 'Enter you\'r name',
+		  :date => 'Enter you\'r date',
+		  :phone => 'Enter you\'r number phone'}
+	hh.each do |key, value|
+		if params[key] == ''
+			@error = hh[key]
+			return erb :visit
+		end
+	end
+		
 	f = File.open './public/user.txt', 'a'
 	f.write "user: #{@username}\n phone:#{@phone}\n dite & time #{@date}\n 
 the hairdresser #{@pric}\n\n"
